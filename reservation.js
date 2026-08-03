2bcc903efc1dc21416eab1244ec9cf6c273a1b08
CDiyor <bchristianmax@gmail.com>
2026-08-03 11:01:07 -0400

    3e reconstructure pour vercel

--- reservation.js
+++ reservation.js
@@ -12,7 +12,7 @@ bookForm.addEventListener('submit', async (e) => {
   const data = Object.fromEntries(new FormData(bookForm).entries());
 
   try {
-    const res = await fetch('https://tacti-shine.onrender.com/api/reserver', {
+    const res = await fetch('/api/reserver', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
